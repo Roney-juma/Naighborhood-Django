dd2b3231d2db00eb75cb1d8e7246653f9f3f9be5
Roney-juma <ochirone10@gmail.com>
2021-07-26 10:32:34 +0300

    View for the authentications

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .forms import *
+from django.shortcuts import redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from . models import *
+from django.views import generic
 
-# Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    mylocs = Myloc.objects.all()
+    return render(request, 'home.html',{"mylocs":mylocs,})
+
+@login_required(login_url='accounts/login/')
+def add_profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(id = current_user.id)
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            caption = form.save(commit=False)
+            caption.user = current_user
+            caption.save()
+            return redirect('myprofile')
+    else:
+        form = NewProfileForm()
+    return render(request, 'edit.html', {"form":form})    
+
