881564b706026b45f2db1702e0369f048540d3b6
Roney-juma <ochirone10@gmail.com>
2021-07-27 09:19:08 +0300

    working on the styles

--- neighbors/settings.py
+++ neighbors/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'neighborhood',
-    'bootstrap4'
+    'bootstrap4',
+    # 'bootstrap3'
 ]
 
 MIDDLEWARE = [
